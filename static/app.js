e97f5e28aa897dbf4676a3a1196282ed03fa6415
Joel Ward <jmward@gmail.com>
2021-06-06 16:11:01 -0500

    update

--- static/app.js
+++ static/app.js
@@ -7,15 +7,13 @@ const GEBI = x=>D.getElementById(x)
 const outElt = GEBI("out")
 const taElt = GEBI("ta")
 const out = x=>outElt.appendChild(TextNode(x+"\n"))
-
-const wsOrigin = 'ws'+location.origin.substr(4)
-
-var ws = new WebSocket(wsOrigin+"/bongo")
-ws.onmessage = e=>clog(["m",e])
+const origin = 'ws'+location.origin.substr(4)
+//var ws = new WebSocket(origin+"/bongo")
+var ws = new WebSocket(origin+"/pubsub")
+ws.onmessage = e=>clog(["m",e])+out("<<"+e.data)
 ws.onerror   = e=>clog(["e",e])
 ws.onclose   = e=>clog(["c",e])
 ws.onopen    = e=>clog(["o",e])
-
 function grab(elt){
     const txt = elt.value
     elt.value = ""
@@ -24,6 +22,5 @@ function grab(elt){
 GEBI("send").onclick=()=>{
     clog("send")
     const txt = grab(taElt)
-    clog(txt)
-    out(txt)
-}
+    out(">>"+txt)
+    ws.send(txt)}
