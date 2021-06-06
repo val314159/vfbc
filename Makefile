6db62fe59be92857782987cf1f92976404e767ae
Joel Ward <jmward@gmail.com>
2021-06-06 14:45:33 -0500

    update

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 C=c/
 B=$C-1/-1/block.txt
 D=$C-1/-1/nxt/
-E=$C-1/-1/xtn/
 call: clean all
 all: c
+	c/-1/-1/nxt/launch.ros
 c:
-	mkdir -p $D $E
+	mkdir -p $D
 	touch $B
 	cp -rp launch.ros $D
 	cp -rp v*-*.lisp  $D
 	cp -rp static/    $C/static/
-	tree -sFl c
+	tree -sFl $C
 clean:
 	rm -fr ? *~
 	tree -sFl .
--- static/app.js
+++ static/app.js
@@ -1,1 +1,29 @@
 // app.js
+const clog = console.log
+const D=document
+const TextNode = x=>D.createTextNode(x)
+const Element = x=>D.createElement(x)
+const GEBI = x=>D.getElementById(x)
+const outElt = GEBI("out")
+const taElt = GEBI("ta")
+const out = x=>outElt.appendChild(TextNode(x+"\n"))
+
+const wsOrigin = 'ws'+location.origin.substr(4)
+
+var ws = new WebSocket(wsOrigin+"/bongo")
+ws.onmessage = e=>clog(["m",e])
+ws.onerror   = e=>clog(["e",e])
+ws.onclose   = e=>clog(["c",e])
+ws.onopen    = e=>clog(["o",e])
+
+function grab(elt){
+    const txt = elt.value
+    elt.value = ""
+    elt.focus()
+    return txt}
+GEBI("send").onclick=()=>{
+    clog("send")
+    const txt = grab(taElt)
+    clog(txt)
+    out(txt)
+}
